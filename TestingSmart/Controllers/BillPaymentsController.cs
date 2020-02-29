3c01858e391fd821bbe0910ea50eff1a9f9d9d9e
Ashutosh <ashutosh322905010@gmail.com>
2020-02-29 15:44:03 +0545

    some changed

--- TestingSmart/Controllers/BillPaymentsController.cs
+++ TestingSmart/Controllers/BillPaymentsController.cs
@@ -127,7 +127,7 @@ namespace TestingSmart.Controllers
 
 
 
-                var item = 2;
+                var item = 3;
                 var billAmountData = _context.Units.Where(x => x.UserId == userIds.Id).OrderByDescending(x => x.Id).FirstOrDefault();
                 if (billAmountData == null)
                 {
